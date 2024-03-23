2de94d04099c1c80e31780d6299523acc146fd44
Indra Ikhsani <kingindra6@gmail.com>
2024-03-23 20:34:59 +0700

    First Commit

---
+++ soal1.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+func calculateAvg(numbers ...int) float64 {
+	result := 0
+	for _, number := range numbers {
+		result += number
+	}
+	avg := float64(result) / float64(len(numbers))
+	return avg
+}
+
+func main() {
+
+	dolphin1 := calculateAvg(96, 108, 89)
+	koala1 := calculateAvg(88, 91, 110)
+	dolphinBonus1 := calculateAvg(97, 112, 101)
+	koalaBonus1 := calculateAvg(109, 95, 123)
+	dolphinBonus2 := calculateAvg(97, 112, 101)
+	koalaBonus2 := calculateAvg(109, 95, 106)
+	minimumScore := 100
+
+	if dolphin1 < koala1 {
+		fmt.Printf("Pemenangnya Koala: %.2f \n", koala1)
+	} else if dolphin1 > koala1 {
+		fmt.Printf("Pemenangnya Dolphin : %.2f \n", dolphin1)
+	} else {
+		fmt.Printf("Hasil Seri tidak ada yang menang \n")
+	}
+
+	if dolphinBonus1 > float64(minimumScore) && koalaBonus1 > float64(minimumScore) {
+		if dolphinBonus1 < koalaBonus1 {
+			fmt.Printf("Pemenangnya Koala: %.2f \n", koalaBonus1)
+		} else if dolphinBonus1 > koalaBonus1 {
+			fmt.Printf("Pemenangnya Dolphin : %.2f \n", dolphinBonus1)
+		}
+	} else {
+		fmt.Printf("Hasil Seri tidak ada yang menang karena score lebih kecil 100 \n")
+	}
+
+	if dolphinBonus2 >= float64(minimumScore) && koalaBonus2 >= float64(minimumScore) {
+		if dolphinBonus2 < koalaBonus2 {
+			fmt.Printf("Pemenangnya Koala: %.2f \n", koalaBonus2)
+		} else if dolphinBonus2 > koalaBonus2 {
+			fmt.Printf("Pemenangnya Dolphin : %.2f \n", dolphinBonus2)
+		} else {
+			fmt.Printf("Hasil Seri tidak ada yang menang karena score lebih besar 100 \n")
+		}
+	} else {
+		fmt.Println("Tidak ada tim yang memenangkan trofi")
+	}
+}
---
+++ soal2.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func bmi(massa float64, tinggi float64) float64 {
+	bmi := massa / (tinggi * tinggi)
+	return bmi
+}
+
+func main() {
+
+	mark1 := bmi(78, 1.69)
+	john1 := bmi(92, 1.95)
+	markHigherBMI1 := mark1 > john1
+	fmt.Printf("Hasil BMI mark 1: %.2f \n", mark1)
+	fmt.Printf("Hasil BMI john 1: %.2f \n", john1)
+	fmt.Println("Mark memiliki BMI lebih tinggi dari John : ", markHigherBMI1)
+
+	mark2 := bmi(95, 1.88)
+	john2 := bmi(85, 1.76)
+	markHigherBMI2 := mark2 > john2
+	fmt.Printf("Hasil BMI mark 1: %.2f \n", mark2)
+	fmt.Printf("Hasil BMI john 1: %.2f \n", john2)
+	fmt.Println("Mark memiliki BMI lebih tinggi dari John : ", markHigherBMI2)
+
+}
